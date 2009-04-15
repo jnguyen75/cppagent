94818153533c6621a7a72e8e0acba1ad6353375d
unknown <Administrator@.(none)>
2009-04-15 16:49:14 +0100

    First working version of windows port

--- win32/lib/glibmm/ustring.h
+++ win32/lib/glibmm/ustring.h
@@ -2,10 +2,12 @@
 #ifndef GLIBMM_USTRING
 #define GLIBMM_USTRING
 
+#include <libxml++config.h>
+
 #include <string>
 
 namespace Glib {
-	class ustring : public std::string {
+	class LIBXMLPP_API ustring : public std::string {
 	public:
 		ustring ( ) : std::string() { }
 		ustring ( const ustring& str ) : std::string(str) { }
@@ -13,7 +15,8 @@ namespace Glib {
 		ustring ( const char * s, size_t n ) : std::string(s, n) { }
 		ustring ( const char * s ) : std::string(s) { }
 		ustring ( size_t n, char c ) : std::string(n, c) { }
-		ustring ( std::string &str ) : std::string(str) { }
+		ustring ( std::string &str );
+		ustring (const std::string &str ) : std::string(str) { }
 		template<class InputIterator> ustring (InputIterator begin, InputIterator end) : std::string(begin, end) { }
 
 		size_type bytes() const { return length(); }
