a8d172e524009baa1e4d826855394ebb9b41fab2
Jimmy <jimmy@DGJ3X3B1.(none)>
2009-04-16 05:54:09 -0700

    Changed the xmlparsing a little for optimization.  Added more test cases.

---
+++ test/agent/xml_parser_test.cpp
@@ -0,0 +1,92 @@
+/*
+* Copyright (c) 2008, AMT – The Association For Manufacturing Technology (“AMT”)
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the AMT nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* DISCLAIMER OF WARRANTY. ALL MTCONNECT MATERIALS AND SPECIFICATIONS PROVIDED
+* BY AMT, MTCONNECT OR ANY PARTICIPANT TO YOU OR ANY PARTY ARE PROVIDED "AS IS"
+* AND WITHOUT ANY WARRANTY OF ANY KIND. AMT, MTCONNECT, AND EACH OF THEIR
+* RESPECTIVE MEMBERS, OFFICERS, DIRECTORS, AFFILIATES, SPONSORS, AND AGENTS
+* (COLLECTIVELY, THE "AMT PARTIES") AND PARTICIPANTS MAKE NO REPRESENTATION OR
+* WARRANTY OF ANY KIND WHATSOEVER RELATING TO THESE MATERIALS, INCLUDING, WITHOUT
+* LIMITATION, ANY EXPRESS OR IMPLIED WARRANTY OF NONINFRINGEMENT,
+* MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE. 
+
+* LIMITATION OF LIABILITY. IN NO EVENT SHALL AMT, MTCONNECT, ANY OTHER AMT
+* PARTY, OR ANY PARTICIPANT BE LIABLE FOR THE COST OF PROCURING SUBSTITUTE GOODS
+* OR SERVICES, LOST PROFITS, LOSS OF USE, LOSS OF DATA OR ANY INCIDENTAL,
+* CONSEQUENTIAL, INDIRECT, SPECIAL OR PUNITIVE DAMAGES OR OTHER DIRECT DAMAGES,
+* WHETHER UNDER CONTRACT, TORT, WARRANTY OR OTHERWISE, ARISING IN ANY WAY OUT OF
+* THIS AGREEMENT, USE OR INABILITY TO USE MTCONNECT MATERIALS, WHETHER OR NOT
+* SUCH PARTY HAD ADVANCE NOTICE OF THE POSSIBILITY OF SUCH DAMAGES.
+*/
+
+#include "xml_parser_test.hpp"
+
+// Registers the fixture into the 'registry'
+CPPUNIT_TEST_SUITE_REGISTRATION(XmlParserTest);
+
+void XmlParserTest::setUp()
+{
+  try
+  {
+    a = new XmlParser("../include/test.xml");
+  }
+  catch (std::exception & e)
+  {
+    CPPUNIT_FAIL("Could not locate test xml: ../include/test.xml");
+  }
+}
+
+void XmlParserTest::tearDown()
+{
+  delete a;
+}
+
+void XmlParserTest::testConstructor()
+{
+  CPPUNIT_ASSERT_THROW(new XmlParser("../include/badPath.xml"), std::exception);
+  CPPUNIT_ASSERT_NO_THROW(new XmlParser("../include/test.xml"));
+}
+
+void XmlParserTest::testGetters()
+{
+  std::list<Device *> devices = a->getDevices();
+  
+  CPPUNIT_ASSERT_EQUAL((size_t) 1, devices.size());
+  
+  std::list<DataItem *> dataItems = a->getDataItems();
+  
+  CPPUNIT_ASSERT_EQUAL((size_t) 17, dataItems.size());
+  
+  
+  bool hasExec = false, hasZcom = false;
+  
+  std::list<DataItem *>::iterator dataItem;
+  for (dataItem=dataItems.begin(); dataItem!=dataItems.end(); dataItem++)
+  {
+    if ((*dataItem)->getId() == 24 and (*dataItem)->getName() == "execution")
+    {
+      hasExec = true;
+    }
+    
+    if ((*dataItem)->getId() == 19 and (*dataItem)->getName() == "Zcom")
+    {
+      hasZcom = true;
+    }
+  }
+  
+  CPPUNIT_ASSERT(hasExec);
+  CPPUNIT_ASSERT(hasZcom);
+}
+
